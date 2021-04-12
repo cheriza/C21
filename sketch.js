6dfa52bc61522e3344f45cea55bbd2ba2bca9070
cheriza <77973656+cheriza@users.noreply.github.com>
2021-04-12 17:52:11 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,42 @@
+
+var fixedRect, movingRect;
+var gameObject1, gameObject2;
+var box
+
+function setup() {
+  createCanvas(1200,800);
+ 
+ 
+  movingRect = createSprite(800, 400,80,30);
+  movingRect.shapeColor = "green";
+
+  fixedRect = createSprite(100, 100, 50, 50);
+  fixedRect.shapeColor = "green";
+  
+  box = createSprite(500, 500, 30, 50);
+  box.shapeColor = "green";
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,box)){
+    movingRect.shapeColor = "blue";
+    box.shapeColor = "blue";
+  }
+  
+  else {
+    movingRect.shapeColor = "green";
+  
+    box.shapeColor = "green"
+  }
+ 
+
+//bounceOff(movingRect,fixedRect)
+  drawSprites();
+}
+
+
+
